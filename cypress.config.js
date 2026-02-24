d1c7adfdaf02f8a42063bcfa99babdf06e281d85
nathanmiller93 <nathanmiller12@hotmail.co.uk>
2026-02-24 20:01:31 +0000

    Tidy up

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,5 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.northumberlandcva.org.uk',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 });
